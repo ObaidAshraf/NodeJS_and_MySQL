ae911204df4bcf92edb592303af606fa420f87e2
ObaidAshraf <ubaidashraf91@gmail.com>
2018-10-15 18:14:54 +0500

    Added SQL module and created connection

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var app = express();
 var path = require('path')
+var mysql = require('mysql')
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/assets'));
+
+var connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    database: 'lifeline',
+})
+
+connection.connect();
+
+connection.query('SELECT * from donors', function (error, results, fields) {
+    if (error) throw error.message;
+    console.log('The solution is: ', results);
+  });
+   
+  connection.end();
 
 app.get('/', function(req, res, next) {
     //res.sendFile(path.join(__dirname + "/views/donors.html"))
