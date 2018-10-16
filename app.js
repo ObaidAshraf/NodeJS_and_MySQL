4912fae2210860c00536ab5ff44ed0a25f67d174
ObaidAshraf <ubaidashraf91@gmail.com>
2018-10-16 17:28:28 +0500

    Added functionality to read from Database and display on donors page

--- app.js
+++ app.js
@@ -5,24 +5,23 @@ var mysql = require('mysql')
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/assets'));
 
-var connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    database: 'lifeline',
-})
 
-connection.connect();
 
-connection.query('SELECT * from donors', function (error, results, fields) {
-    if (error) throw error.message;
-    console.log('The solution is: ', results);
-  });
-   
-  connection.end();
-
-app.get('/', function(req, res, next) {
-    //res.sendFile(path.join(__dirname + "/views/donors.html"))
-    res.render("donors")
+app.get('/donors', function(req, res, next) {
+    var connection = mysql.createConnection({
+        host: '127.0.0.1',
+        user: 'root',
+        database: 'lifeline',
+    })    
+    connection.connect();
+    connection.query('SELECT * from donors', function (error, results, fields) {
+        if (error) throw error.message;
+        console.log('The solution is: ', results);
+        res.render("donors", {
+            data: results,
+        })
+    });
+    connection.end();
 })
 
 
